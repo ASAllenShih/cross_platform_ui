8ebcc2426de3ccc2eff9dfa67488996d993e1355
AS_Allen_Shih <65409535+ASAllenShih@users.noreply.github.com>
2026-01-19 19:21:37 +0800

    feat: 更新 dialogShow 方法以支持跨平台類型的對話框顯示

--- lib/cross_platform/dialog/show.dart
+++ lib/cross_platform/dialog/show.dart
@@ -1,19 +1,21 @@
-import 'package:fluent_ui/fluent_ui.dart';
+import 'package:cross_platform_ui/cross_platform_type.dart';
+import 'package:cross_platform_ui/cross_platform_ui.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart' as material_widgets show showDialog;
 import 'package:fluent_ui/fluent_ui.dart' as fluent_widgets show showDialog;
-import 'package:flutter/material.dart';
 
 Future<T?> dialogShow<T>({
   required BuildContext context,
   required Widget Function(BuildContext) builder,
   bool? barrierDismissible,
+  CrossPlatformType? type,
 }) {
-  if (builder is AlertDialog Function(BuildContext)) {
+  type ??= CrossPlatformUi.typeDefault;
+  if (type == CrossPlatformType.material) {
     return material_widgets.showDialog<T>(context: context, builder: builder);
-  } else if (builder is CupertinoAlertDialog Function(BuildContext)) {
+  } else if (type == CrossPlatformType.cupertino) {
     return showCupertinoDialog<T>(context: context, builder: builder);
-  } else if (builder is ContentDialog Function(BuildContext)) {
+  } else if (type == CrossPlatformType.fluent) {
     return fluent_widgets.showDialog<T>(context: context, builder: builder);
   } else {
     throw Exception('Unsupported dialog builder type');
